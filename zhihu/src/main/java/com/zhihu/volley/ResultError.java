01b23880707b589c87b66c0cc9b1283b2d260382
unknown <bjweilingzi@BIH-D-4451.netease.internal>
2016-01-08 18:37:02 +0800

    add volley

---
+++ zhihu/src/main/java/com/zhihu/volley/ResultError.java
@@ -0,0 +1,15 @@
+package com.netease.vopen.volley;
+
+/**
+ * Created by libowen on 2015/8/21.
+ */
+public class ResultError extends VolleyError {
+    public int errorCode;
+    public String errorMsg;
+
+    public ResultError(int errorCode, String errorMsg) {
+        super();
+        this.errorCode=errorCode;
+        this.errorMsg=errorMsg;
+    }
+}
---
+++ zhihu/src/main/java/com/zhihu/volley/toolbox/MultipartRequest.java
@@ -0,0 +1,104 @@
+package com.netease.vopen.volley.toolbox;
+
+import com.netease.vopen.net.mutipart.MultipartEntity;
+import com.netease.vopen.util.log.PalLog;
+import com.netease.vopen.volley.AuthFailureError;
+import com.netease.vopen.volley.NetworkResponse;
+import com.netease.vopen.volley.Request;
+import com.netease.vopen.volley.Response;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by libowen on 2015/7/16.
+ */
+public class MultipartRequest extends Request<String> {
+
+    private MultipartEntity mMultiPartEntity;
+    Map<String, String> mHeaders = new HashMap<String, String>();
+
+    private final Response.Listener<String> mListener;
+
+    /**
+     * Creates a new request with the given url.
+     *
+     * @param url URL to fetch the string at
+     * @param listener Listener to receive the String response
+     */
+    public MultipartRequest(String url, Response.Listener<String> listener) {
+        this(url, listener, null);
+    }
+
+    /**
+     * Creates a new POST request.
+     *
+     * @param url URL to fetch the string at
+     * @param listener Listener to receive the String response
+     * @param errorListener Error listener, or null to ignore errors
+     */
+    public MultipartRequest(String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        super(Method.POST, url, errorListener);
+        mListener = listener;
+    }
+
+    /**
+     * @return
+     */
+    public MultipartEntity getMultiPartEntity() {
+        return mMultiPartEntity;
+    }
+
+    public void setMultiPartEntity(MultipartEntity multiPartEntity) {
+         mMultiPartEntity=multiPartEntity;
+    }
+
+    @Override
+    public String getBodyContentType() {
+        return mMultiPartEntity.getContentType().getValue();
+    }
+
+    public void addHeader(String key, String value) {
+        mHeaders.put(key, value);
+    }
+
+    @Override
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        return mHeaders;
+    }
+
+    @Override
+    public byte[] getBody() {
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            // multipart body
+            mMultiPartEntity.writeTo(bos);
+        } catch (IOException e) {
+            PalLog.e("MultipartRequest", "IOException writing to ByteArrayOutputStream");
+        }
+        return bos.toByteArray();
+    }
+
+    @Override
+    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+        String parsed = "";
+        try {
+            parsed = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+        } catch (UnsupportedEncodingException e) {
+            parsed = new String(response.data);
+        }
+        return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response));
+    }
+
+    @Override
+    protected void deliverResponse(String response) {
+        if (mListener != null) {
+            mListener.onResponse(response);
+        }
+    }
+
+}
