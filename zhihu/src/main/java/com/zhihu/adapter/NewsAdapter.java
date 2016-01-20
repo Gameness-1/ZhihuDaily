45c5ed3854aa802639d56e7746aa9f0e89088b07
linzi <bjweilingzi@BIH-D-4451.netease.internal>
2016-01-20 11:50:53 +0800

    Revert "aaa"
    This reverts commit c793bab7bdbcb3ba202c8dd1760b6cbc376c0ca2.

--- zhihu/src/main/java/com/zhihu/adapter/NewsAdapter.java
+++ zhihu/src/main/java/com/zhihu/adapter/NewsAdapter.java
@@ -71,7 +71,6 @@ public class NewsAdapter extends BaseAdapter {
         News newItem = getItem(position);
         holder.title.setText(newItem.getTitle());
         holder.image.setImageURI(Uri.parse(newItem.getImgUrl()));
-        ///
         return convertView;
     }
 
